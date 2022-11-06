410ae43efdd5b23db6156bc2566135ff61e83298
10Sadique <jafarsjahan@gmail.com>
2022-11-07 01:00:57 +0600

    AuthContext complete

--- src/contexts/AuthProvider.tsx
+++ src/contexts/AuthProvider.tsx
@@ -1,27 +1,77 @@
-import { createContext, FC, ReactNode, useState } from 'react';
+import { createContext, FC, ReactNode, useEffect, useState } from 'react';
 import app from '../firebase/firebase.config';
-import { getAuth, User } from 'firebase/auth';
+import {
+    createUserWithEmailAndPassword,
+    getAuth,
+    onAuthStateChanged,
+    signInWithEmailAndPassword,
+    signOut,
+    updateProfile,
+    User,
+    UserCredential,
+} from 'firebase/auth';
+import { sign } from 'crypto';
 
 type IAuthProvider = {
     children: ReactNode;
 };
 
 export interface IAuthContext {
-    user: User | undefined;
+    user: User | null;
     loading: boolean;
+    signIn: (email: string, password: string) => Promise<UserCredential>;
+    signUp: (email: string, password: string) => Promise<UserCredential>;
+    updateUser: (name: string, image: string) => Promise<void> | undefined;
+    logOut: () => Promise<void>;
 }
 
 const auth = getAuth(app);
 
-export const AuthContext = createContext<IAuthContext | undefined>(undefined);
+export const AuthContext = createContext<IAuthContext | null>(null);
 
 const AuthProvider: FC<IAuthProvider> = ({ children }) => {
-    const [user, setUser] = useState<User>();
+    const [user, setUser] = useState<User | null>(null);
     const [loading, setLoading] = useState<boolean>(true);
+
+    const signIn = (email: string, password: string) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    };
+
+    const signUp = (email: string, password: string) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    };
+
+    const updateUser = (name: string, image: string) => {
+        setLoading(true);
+        if (user !== null)
+            return updateProfile(user, { displayName: name, photoURL: image });
+    };
+
+    const logOut = () => {
+        setLoading(true);
+        return signOut(auth);
+    };
+
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, (presentUser) => {
+            setUser(presentUser);
+            setLoading(false);
+        });
+
+        return () => {
+            unSubscribe();
+        };
+    }, []);
 
     const authInfo: IAuthContext = {
         user,
         loading,
+        signIn,
+        signUp,
+        updateUser,
+        logOut,
     };
 
     return (
