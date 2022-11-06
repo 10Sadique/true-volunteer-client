34c54c8243a07cdcc12b728461c0d0a679dc3950
10Sadique <jafarsjahan@gmail.com>
2022-11-06 15:04:06 +0600

    homepage  added

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,4 +1,11 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Main from '../layout/Main';
+import Home from '../pages/Home';
 
-export const routes = createBrowserRouter([{ path: '/', element: <Main /> }]);
+export const routes = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main />,
+        children: [{ path: '/', element: <Home /> }],
+    },
+]);
