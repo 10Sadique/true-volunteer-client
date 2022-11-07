1580a4db8f84ae7a3d8a54be023caf623f1f8724
10Sadique <jafarsjahan@gmail.com>
2022-11-07 14:08:23 +0600

    sing out functionality complete

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,47 +1,83 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import { NavLink } from 'react-router-dom';
 import { FaBars, FaTimes } from 'react-icons/fa';
-
-const navLinks = [
-    <div
-        className="flex flex-col w-full gap-5 font-semibold md:items-center md:flex-row"
-        key={1}
-    >
-        <NavLink
-            className={({ isActive }) => (isActive ? 'text-cyan-600' : '')}
-            to={`/home`}
-        >
-            Home
-        </NavLink>
-        <NavLink
-            className={({ isActive }) => (isActive ? 'text-cyan-600' : '')}
-            to={`/events`}
-        >
-            Events
-        </NavLink>
-        <NavLink
-            className={({ isActive }) => (isActive ? 'text-cyan-600' : '')}
-            to={`/activities`}
-        >
-            Activities
-        </NavLink>
-        <NavLink
-            className={({ isActive }) => (isActive ? 'text-cyan-600' : '')}
-            to={`/signin`}
-        >
-            Sing In
-        </NavLink>
-        <NavLink
-            className={({ isActive }) => (isActive ? 'text-cyan-600' : '')}
-            to={`/signup`}
-        >
-            Sign Up
-        </NavLink>
-    </div>,
-];
+import { AuthContext, IAuthContext } from '../contexts/AuthProvider';
 
 const Navbar = () => {
     const [navbar, setNavbar] = useState<boolean>(false);
+    const { user, logOut } = useContext(AuthContext) as IAuthContext;
+
+    const handleSignOut = () => {
+        logOut()
+            .then(() => {
+                console.log('Signed Out');
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+    };
+
+    const navLinks = [
+        <div
+            className="flex flex-col w-full gap-5 font-semibold md:items-center md:flex-row"
+            key={1}
+        >
+            <NavLink
+                className={({ isActive }) => (isActive ? 'text-cyan-600' : '')}
+                to={`/home`}
+            >
+                Home
+            </NavLink>
+            <NavLink
+                className={({ isActive }) => (isActive ? 'text-cyan-600' : '')}
+                to={`/events`}
+            >
+                Events
+            </NavLink>
+            {user?.uid ? (
+                <>
+                    <NavLink
+                        className={({ isActive }) =>
+                            isActive ? 'text-cyan-600' : ''
+                        }
+                        to={`/activities`}
+                    >
+                        My Activities
+                    </NavLink>
+                    <button onClick={handleSignOut}>Sign Out</button>
+                    <div className="h-10 w-10 rounded-full overflow-hidden bg-cyan-200">
+                        {user?.photoURL && (
+                            <img
+                                className="w-full h-full object-cover"
+                                src={user.photoURL}
+                                alt=""
+                            />
+                        )}
+                    </div>
+                </>
+            ) : (
+                <>
+                    <NavLink
+                        className={({ isActive }) =>
+                            isActive ? 'text-cyan-600' : ''
+                        }
+                        to={`/signin`}
+                    >
+                        Sing In
+                    </NavLink>
+                    <NavLink
+                        className={({ isActive }) =>
+                            isActive ? 'text-cyan-600' : ''
+                        }
+                        to={`/signup`}
+                    >
+                        Sign Up
+                    </NavLink>
+                </>
+            )}
+        </div>,
+    ];
+
     return (
         <nav className="sticky top-0 z-50 w-full text-gray-700 shadow-md bg-white/70 backdrop-blur-md">
             <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8 ">
